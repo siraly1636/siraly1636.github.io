ce37a3c79fd644f896ffc86d28a14495f6ee1611
Varga András <andras390219@gmail.com>
2021-10-09 16:52:15 +0200

    test 1

--- elefant/assets/js/elefant.js
+++ elefant/assets/js/elefant.js
@@ -4,4 +4,14 @@ fetch("assets/json/elefant.json")
 
 function elefantJson(adatok){
     console.log(adatok);
+
+    for (elem of adatok){
+        sz+="<tr>";
+        sz+="<td>";
+        sz+=elem.name;
+        sz+="</td>";
+        sz+="</tr>";
+ 
+    }
+    document.getElementById("torzs").innerHTML=sz;
 }
