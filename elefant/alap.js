f1cc41c724f7ce1d0d1e09e0aa384c653859122a
Varga András <andras390219@gmail.com>
2021-10-10 11:39:03 +0200

    Update

---
+++ elefant/alap.js
@@ -0,0 +1,19 @@
+fetch("elefant.json")
+.then(x => x.json())
+.then(y => elefantJson(y));
+
+function elefantJson(adatok){
+    //console.log(adatok);
+
+    var sz="";
+    for (elem of adatok){
+        sz+="<div class='col-sm-4 text-center box'><h4 class='elefant-name'>"+elem.name+"</h4>";
+        sz+="<a target='_blank' href='"+elem.image+"'><img src='"+elem.image+"' class='img-size'></a>";
+        if (elem.wikilink == "Unavailable") {
+            sz+="<p class='link'>Nem elérhető</p></div>"
+        }else{
+            sz+="<p class='link'><a target='_blank' href='"+elem.wikilink+"'>Wiki</a></p></div>" 
+        }
+    }
+    document.getElementById("elefants").innerHTML=sz;
+}
